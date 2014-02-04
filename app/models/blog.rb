f8c0d3d754806de85e7abae8b53502658710b551
Andy Hayes <andythemanhayes@hotmail.com>
2014-02-04 13:25:46 -0700

    updates some CSS, styled buttons and adds shadows

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,8 @@ class Blog < ActiveRecord::Base
   attr_accessible :author, :body, :featured, :published_at, :title
 
 has_many :comments
+
+validates :author, :title, :body, presence:true
+validates :title, length:{in: 6..72}
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :blog
   attr_accessible :author, :message
+  validates :author, :message, presence:true
 end
