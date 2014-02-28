eec0596ca25be8c44b13be3d5ff3aac0b54ab1f4
Andy Hayes <andythemanhayes@hotmail.com>
2014-02-28 14:04:19 -0700

    remove project_picture replaced it with polymorphic pics

--- app/models/project_picture.rb
+++
@@ -1,8 +0,0 @@
-class ProjectPicture < ActiveRecord::Base
-  belongs_to :picable, polymorphic: true
-  attr_accessible :name
-   has_attached_file :project_picture, 
-  styles: {large: "600x600>", 
-  			medium: "300x300>", 
-  			thumb: "50x50>"}
-end
